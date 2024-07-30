ca9f0b7ad275a645ee1501a3dfbb171dde601a65
Danielle Bassetto <danibbassetto@hotmail.com>
2024-07-29 23:58:20 -0300

    Adicionar arquivos de projeto.

---
+++ JornadaMilhas.Test.Integracao/ContextoCollection.cs
@@ -0,0 +1,12 @@
+﻿namespace JornadaMilhas.Test.Integracao;
+
+[CollectionDefinition(nameof(ContextoCollection))]
+public class ContextoCollection : ICollectionFixture<ContextoFixture>
+{
+}
+
+/*
+O que aconteceu? Utilizamos dois recursos do xUnit, o ClassFixture para compartilhar recursos dentro da mesma classe e o CollectionFixture para compartilhar recursos quando temos mais de uma classe diferente, que utiliza o mesmo recurso.
+
+Com isso, conseguimos otimizar bastante a utilização da conexão com o banco, porque não precisamos mais ficar criando e desfazendo uma conexão todas as vezes que executamos um teste. Na próxima aula, vamos continuar avançando em testes de integração. Até lá. 
+*/
---
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -0,0 +1,16 @@
+﻿using JornadaMilhas.Dados;
+using Microsoft.EntityFrameworkCore;
+
+namespace JornadaMilhas.Test.Integracao;
+
+public class ContextoFixture
+{
+    public JornadaMilhasContext Context { get; }
+
+    public ContextoFixture()
+    {
+        var options = new DbContextOptionsBuilder<JornadaMilhasContext>().UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False").Options;
+
+        Context = new JornadaMilhasContext(options);
+    }
+}
